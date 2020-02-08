a08c579557abdf4c5d8b60a08ed9aa50a5a1498c
methias <tmn5@pm.me>
2020-02-08 15:13:02 +0200

    add main app component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,40 @@
+import './styles.css'
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { useMachine } from './redditMachine';
+import { Subreddit } from './Subreddit';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const subreddits = ['frontend', 'reactjs', 'vuejs'];
+
+const App = () => {
+	const [current, send] = useMachine(redditMachine);
+	const { subreddit } = current.context;
+
+	return (
+		<main
+			data-machine={redditMachine.id}
+			data-state={current.toStrings().join(' ')}
+		>
+			<header>
+				<select
+					onChange={e => {
+						send('SELECT', { name: e.target.value });
+					}}
+				>
+					<option disabled>Select one</option>
+					{subreddits.map(subreddit => {
+						return <option key={subreddit}>{subreddit}</option>
+					})}
+				</select>
+			</header>
+			<div>
+				<h1>{current.matches('idle') ? 'Select a subreddit' : null}</h1>
+				{subreddit && <Subreddit service={subreddit} key={subreddit.id} />}
+			</div>
+		</main>
+	);
+};
+
+const rootElement = document.getElementById('root';)
+ReactDOM.render(<App />, rootElement);
 
