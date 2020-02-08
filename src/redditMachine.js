0fbcc4e51235df18f72303a633c66545eca9026b
methias <tmn5@pm.me>
2020-02-08 14:54:43 +0200

    add reddit machine

---
+++ src/redditMachine.js
@@ -0,0 +1,36 @@
+import { Machine, assign, spawn } from 'xstate';
+import { createSubredditMachine } from './subredditMachine';
+
+export const redditMachine = Machine({
+	id: 'reddit',
+	context: {
+		subreddits: {},
+		subreddit: null
+	},
+	states: { idle: {}, selected: {} },
+	on: {
+		SELECT: {
+			target: '.selected',
+			actions: assign((context, event) => {
+				let subreddit = context.subreddits[event.name];
+
+				if (subreddit) {
+					return {
+						...context,
+						subreddit
+					};
+				}
+
+				subreddit = spawn(createSubredditMachine(event.name));
+
+				return {
+					subreddits: {
+						...context.subreddits,
+						[event.name]: subreddit
+					},
+					subreddit
+				};
+			})
+		}
+	}
+});
